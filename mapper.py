b8297799ccbdd55d0128edf59bf080912dc9900c
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-05-25 04:53:11 +0200

    test mapper file

--- mapper.py
+++ mapper.py
@@ -8,9 +8,9 @@ for line in sys.stdin:
 	#removing leading and trailing whitespaces
 	line = line.strip()
 	#checking if the line is blank before loading it as json
-	if not line:continue #reading in the given json file 
+	if not line:#reading in the given json file
 		tweet = json.loads(line)
-		if not tweet.get('text'):continue
+		if not tweet.get('text'):
 			word_list = {'han', 'hon', 'den', 'det', 'denna', 'denne', 'hen'}
 			word_count = {}
 			if not tweet['retweeted']:
