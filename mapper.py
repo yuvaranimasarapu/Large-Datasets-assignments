15bafbe0d575e0c4db0cd6f199ef8b2119d7991a
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-05-25 04:24:46 +0200

    reducer mapper python files updated

--- mapper.py
+++ mapper.py
@@ -5,15 +5,20 @@ import sys
 import json
 
 for line in sys.stdin:
-	line = line.strip(); #removing leading and trailing whitespaces
-    if not line:continue #checking if the line is blank before loading it as json
-        tweet = json.loads(line) #reading in the given json file
-		if not tweet.get('text'):continue
-        word_list = {'han', 'hon', 'den', 'det', 'denna', 'denne', 'hen'}
-        word_count = {}
-        if not tweet['retweeted']:
-            text = tweet['text'].lower()
-            for eachword in word_list:
-                if eachword in text:
-                    print('%s\t%s' % (eachword, 1))
-            print('%s\t%s' % ('unique',1))
+	#removing leading and trailing whitespaces
+	line = line.strip()
+	#checking if the line is blank before loading it as json
+    if not line:
+		continue
+		#reading in the given json file
+        tweet = json.loads(line)
+		if not tweet.get('text'):
+			continue
+			word_list = {'han', 'hon', 'den', 'det', 'denna', 'denne', 'hen'}
+			word_count = {}
+			if not tweet['retweeted']:
+				text = tweet['text'].lower()
+				for eachword in word_list:
+					if eachword in text:
+						print('%s\t%s' % (eachword, 1))
+				print('%s\t%s' % ('unique',1))
